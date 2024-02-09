8f1d00ea72f41642f73ec44b84b13182f0626ba3
Viktor Hilmer <hilmer.viktor@gmail.com>
2024-02-09 12:21:14 +0100

    Added environment variables

--- automate_email.py
+++ automate_email.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+import os
 import pytest
 from pytest_bdd import scenarios, given, when, then
 from playwright.sync_api import sync_playwright
+
+load_dotenv()
 
 # Link to the Gherkin feature file for BDD tests
 scenarios('email.feature')
@@ -24,10 +28,10 @@ def page(browser):
 def log_in(page):
     # Login steps to access the email account
     page.goto('https://mail.yahoo.com')
-    page.fill('#login-username', 'playwright.test')
+    page.fill('#login-username', os.environ['PLAYWRIGHT_EMAIL_USERNAME'])
     with page.expect_navigation():
         page.keyboard.press('Enter')
-    page.fill('#login-passwd', 'Schnellecke1!')
+    page.fill('#login-passwd', os.environ['PLAYWRIGHT_EMAIL_PASSWORD'])
     with page.expect_navigation():
         page.keyboard.press('Enter')
 
@@ -50,7 +54,8 @@ def create_and_send_email(page):
     page.set_input_files('input[type="file"]', 'Hilmer_CV.pdf')
     page.click('[data-test-id="rte"]')
     page.type('[data-test-id="rte"]', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis viverra cursus. Quisque id elit ut mauris congue placerat ut in ex. Donec semper lacinia dui vel mattis. In hac habitasse platea dictumst. Donec id imperdiet eros. Nam sagittis est eget imperdiet congue. Nullam non porta odio. Nam eget tempus est. Suspendisse sodales tincidunt nunc, vitae maximus est ultricies aliquet. Donec et velit eros. Nulla facilisi. Donec vel tempor tellus, vel sodales augue. Donec scelerisque sapien ligula, non luctus nisi mattis id.')
-    page.click('button[data-test-id="compose-send-button"]')
+    with page.expect_navigation():
+        page.click('button[data-test-id="compose-send-button"]')
 
 @then('the email with the attachment should be successfully sent')
 def verify_email_sent(page):
